100dcbbe5174cf078fac3e65fa47f15e210e5430
darlinramos <darlinramos981@gmail.com>
2022-08-08 21:12:15 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calcular el area y el volumen de un triangulo
+            Console.Write("Ingrese la base del triangulo: ");
+            int base_t = int.Parse(Console.ReadLine());
+            Console.Write("Ingresar la altura del triangulo: ");
+            int altura = int.Parse(Console.ReadLine());
+            double area = (base_t * altura) / 2;
+            Console.Write("El area del triangulo es: " + area + " cm");
+            Console.ReadKey();
+
+    }
+    }
+}
